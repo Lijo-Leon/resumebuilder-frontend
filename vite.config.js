39241cf4937cfaf1e04df82288edf629d5fe600f
Lijo-Leon <lijoleon2002@gmail.com>
2025-10-16 13:00:47 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  css: {
+    // Force PostCSS-only transformer, disables lightningcss
+    transformer: 'postcss'
+  },
+  optimizeDeps: {
+    exclude: ['lightningcss'] // explicitly exclude LightningCSS
+  }
 })
